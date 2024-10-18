24292b978fc09e2e391549b5606e7f479fa4a20c
raphaelcastrofo <raphaelcastrofo@gmail.com>
2024-10-18 17:19:06 +0200

    app with kotlin and xml for an app that calcuate the investment with interest

---
+++ app/src/main/java/com/example/investeasy/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.investeasy
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val edtContribution = findViewById<TextInputEditText>(R.id.tie_contribution)
+        val edtTime = findViewById<TextInputEditText>(R.id.tie_time)
+        val edtInterest = findViewById<TextInputEditText>(R.id.tie_interest)
+        val btnCalculate = findViewById<Button>(R.id.btn_calculate)
+
+
+        val tvResult = findViewById<TextView>(R.id.tv_result)
+        val tvYield = findViewById<TextView>(R.id.tv_yielded_value)
+
+        btnCalculate.setOnClickListener {
+
+            val contributionStr: String = edtContribution.text.toString()
+            val timeStr: String = edtTime.text.toString()
+            val interestStr: String = edtInterest.text.toString()
+
+            if (contributionStr == "" || timeStr == "") {
+                //show mensage to the user
+                Snackbar
+                    .make(
+                        edtContribution,
+                        "fill in all the fields",
+                        Snackbar.LENGTH_LONG
+                    )
+                    .show()
+            } else {
+
+
+                val contribution: Float = contributionStr.toFloat()
+                val time: Float = timeStr.toFloat()
+                val interest: Float = interestStr.toFloat()
+
+                val totalContribution: Float = time * contribution
+                val total : Float = totalContribution * interest
+
+                val yield = total - totalContribution
+
+                tvResult.text = "$ " + total.toString()
+                 tvYield.text = yield.toString()
+
+
+            }
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
