4cb8d362f715e368edf35a28b2c646587ad1a22b
raphaelcastrofo <raphaelcastrofo@gmail.com>
2024-10-23 19:19:11 +0200

    APP READY!!

--- app/src/main/java/com/example/investeasy/MainActivity.kt
+++ app/src/main/java/com/example/investeasy/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.investeasy
 
+import android.icu.text.NumberFormat
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -8,6 +9,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
+import java.util.Locale
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         val edtTime = findViewById<TextInputEditText>(R.id.tie_time)
         val edtInterest = findViewById<TextInputEditText>(R.id.tie_interest)
         val btnCalculate = findViewById<Button>(R.id.btn_calculate)
+        val btcClear = findViewById<Button>(R.id.btn_clear)
 
 
         val tvResult = findViewById<TextView>(R.id.tv_result)
@@ -51,12 +56,22 @@ class MainActivity : AppCompatActivity() {
 
                 val yield = total - totalContribution
 
-                tvResult.text = "$ " + total.toString()
-                 tvYield.text = yield.toString()
+                val formatter: NumberFormat = NumberFormat.getCurrencyInstance(Locale("pt", "BR"))
+
+                tvResult.text = formatter.format(total)
+                tvYield.text = formatter.format(yield)
 
 
             }
         }
+
+        btcClear.setOnClickListener{
+            contributionStr == ""
+            edtTime == ""
+            edtInterest = ""
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
